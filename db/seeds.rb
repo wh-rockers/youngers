8c814edf2d1399b584772b79229aa3e99865e0e9
apprentice1988 <zilongji@gmail.com>
2014-11-30 23:19:58 +0800

    add industry seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,3 @@
-if StartUp.count == 0
-	StartUp.create([
-		{ 
-			logo_url: 'http://www.encore-cav.com/home-theater/images/images2/logo_icon_large.png',
-			name: '恋爱笔记',
-			likes_count: 0,
-			desc: '致力于解决青年人就业问题， 通过大数据帮助高中生，大学毕业生了解社会上各个职业的情况'
-		},
-		{ 
-			logo_url: 'http://thetechnologygeek.org/wp-content/uploads/2014/02/StumbleUpon-logo-icon.png',
-			name: '人人运动',
-			likes_count: 0,
-			desc: '致力于解决青年人就业问题， 通过大数据帮助高中生，大学毕业生了解社会上各个职业的情况'
-		},
-		{ 
-			logo_url: 'http://fc09.deviantart.net/fs71/i/2012/066/a/c/google_play_icon___logo_by_chrisbanks2-d4s1i75.png',
-			name: '优视网',
-			likes_count: 0,
-			desc: '致力于解决青年人就业问题， 通过大数据帮助高中生，大学毕业生了解社会上各个职业的情况'
-		},
-		])
-end
-
 if Industry.count == 0
 	Industry.create([
 		{ name: '计算机／互联网', abbr: 'IT' },
