e258cd292aae9e671a9971bd4fd49900343eef73
apprentice1988 <zilongji@gmail.com>
2014-12-08 23:50:27 +0800

    finish basic personal function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   include User::Role
+  scope :members, -> { where("'member' = ANY (roles)") }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name, :email, :avatar
