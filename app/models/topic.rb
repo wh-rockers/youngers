f00b012ce6ae9a24d8c80146e39c8af9a6debd6e
apprentice1988 <zilongji@gmail.com>
2015-01-18 23:38:08 +0800

    add dependent to topic and comments

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :content, :user_id, :start_time, :end_time, :location, :attendee_number
-  has_many :topic_comments
+  has_many :topic_comments, dependent: :destroy
   validates_presence_of :organizer, unless: Proc.new { |f| f.personal? }
   def time 
     "#{start_time.to_s(:short)} - #{end_time.to_s(:short)}"
