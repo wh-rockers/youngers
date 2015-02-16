a73d7cf30737514aa92762d5cf54fcc2466410a9
apprentice1988 <zilongji@gmail.com>
2015-02-17 00:44:22 +0800

    add copyright

--- app/controllers/weixin/messages_controller.rb
+++ app/controllers/weixin/messages_controller.rb
@@ -5,12 +5,32 @@ class Weixin::MessagesController < Weixin::ApplicationController
 
   def receive
     if request.post?
-      xml = Hash.from_xml request.body.read
-      p '====================='
-      p xml
-      render text: 'ok'
+      @xml = Hash.from_xml request.body.read
+      p @xml
+      if scan_qr_code_in_register?
+        response_scan_qr_code_in_register
+      end
     else
       render text: params[:echostr]
     end
+  end
+
+  private
+
+  def scan_qr_code_in_register?
+    @xml['EventKey'] == '123'
+  end
+
+  def response_scan_qr_code_in_register
+    if bind_open_id?
+      # sign_in User.find_by(open_id: xml['FromUserName'])
+      render xml: Weixin::Message.new.response_text_xml(@xml['FromUserName'], '登录成功') and return
+    else
+      render xml: Weixin::Message.new.response_text_xml(@xml['FromUserName'], '该微信账号为绑定， 请通过菜单登录') and return
+    end
+  end
+
+  def bind_open_id?
+    User.exists?(open_id: xml['FromUserName'])
   end
 end
--- lib/modules/weixin/message.rb
+++ lib/modules/weixin/message.rb
@@ -1,3 +1,14 @@
 class Weixin::Message
-  
+  SELF_OPEN_ID = 'gh_a584189690a0'
+  def response_text_xml(to_user_id, content)
+    {
+      "xml"=> {
+        "ToUserName"=>SELF_OPEN_ID,
+        "FromUserName"=> to_user_id
+        "CreateTime"=>Time.now.to_i.to_s,
+        "MsgType"=>"text", 
+        "Content" => content
+      }
+    }
+  end
 end
